56deb662f1c10fde1068b0f8b5b570ea9cbc77b5
Alan Morgan <alanxoc3@gmail.com>
2018-05-25 21:24:44 -0600

    basic reading in lua file :)

--- src/bity-8/src/lua.rs
+++ src/bity-8/src/lua.rs
@@ -1,14 +1,22 @@
 extern crate hlua;
 
+use std::fs::File;
+use std::path::Path;
+use std::io::BufReader;
+
 // To see the generated assembly, run:
 // cargo rustc --release --example basic -- --emit=asm
 
-pub fn run_lua_test() {
+pub fn run(file: &Path) {
+    // Create lua virtual machine.
     let mut lua = hlua::Lua::new();
 
-    lua.set("a", 12);
-    let val: i32 = lua.execute(r#"return a * 5;"#).unwrap();
-    println!("{}", val);
+    let f = File::open(file).expect("file not found");
+    let buf_reader = BufReader::new(f);
 
-    assert_eq!(val, 60);
+    lua.execute_from_reader::<(), _>(buf_reader)
+        .expect("something went wrong with running this file.");
+
+    let val: i32 = lua.get("b").unwrap();
+    println!("executed: {}", val);
 }
