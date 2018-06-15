6a652b4e03a7cdad102c57f04d2e825329629ae4
Alan Morgan <alanxoc3@gmail.com>
2018-06-15 11:46:19 -0600

    comment to demo

--- assets/test.lua
+++ assets/test.lua
@@ -8,18 +8,6 @@ for k, v in pairs(colors) do
    _poke(0x40000+k-1, v)
 end
 
-_poke(2, 001)
-_poke(8, 120)
-_poke(10, 0x20)
-_poke(189, -3020)
-
-for i=0, 200 do
-   local val = _peek(i)
-   if val ~= 0 then
-      print("val "..i.." is "..val)
-   end
-end
-
 function _update()
    -- set the screen.
    for i=0, 192-1 do
@@ -30,4 +18,18 @@ function _update()
       _mcpy(0x40400 + 0x60 * j, 0x40400, 0x60)
    end
 
+end
+
+-- The below test should print NOTHING, because this is all in the read-only
+-- section of the cartridge.
+_poke(2, 001)
+_poke(8, 120)
+_poke(10, 0x20)
+_poke(189, -3020)
+
+for i=0, 200 do
+   local val = _peek(i)
+   if val ~= 0 then
+      print("val "..i.." is "..val)
+   end
 end
