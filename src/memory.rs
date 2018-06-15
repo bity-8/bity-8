06d0162b465958ea090000f430b7c4dde0315e03
Alan Morgan <alanxoc3@gmail.com>
2018-06-14 19:18:24 -0600

    Nice alias

--- src/memory.rs
+++ src/memory.rs
@@ -1,20 +1,19 @@
 use std::cmp;
 
-pub type Memory = [i8; 0x50000];
-
-pub static LOC_CART: usize = 0x00000; // Cartridge
-pub static LOC_HARD: usize = 0x40000; // Hardware Config
-pub static LOC_SCRE: usize = 0x40400; // Screen Buffer
-pub static LOC_SPRI: usize = 0x43A00; // Sprite Sheets
-pub static LOC_TILE: usize = 0x47000; // Tile Maps
-pub static LOC_SAVE: usize = 0x4DC00; // Save
-pub static LOC_EMPT: usize = 0x4E000; // Empty
-pub static LOC_MULT: usize = 0x4F800; // Multicart
-pub static LOC_END : usize = 0x50000; // Multicart
+pub const LOC_CART: usize = 0x00000; // Cartridge
+pub const LOC_HARD: usize = 0x40000; // Hardware Config
+pub const LOC_SCRE: usize = 0x40400; // Screen Buffer
+pub const LOC_SPRI: usize = 0x43A00; // Sprite Sheets
+pub const LOC_TILE: usize = 0x47000; // Tile Maps
+pub const LOC_SAVE: usize = 0x4DC00; // Save
+pub const LOC_EMPT: usize = 0x4E000; // Empty
+pub const LOC_MULT: usize = 0x4F800; // Multicart
+pub const LOC_END : usize = 0x50000; // Multicart
+pub type Memory = [i8; LOC_END];
 
 // Gotta do this first.
 pub fn create_memory() -> Memory {
-    [0; 0x50000]
+    [0; LOC_END]
 }
 
 // Maps a vector to memory. This is safe.
