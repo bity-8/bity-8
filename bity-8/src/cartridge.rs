89083ff863825f264fab331f099aea8140d18f18
Alan Morgan <alanxoc3@gmail.com>
2018-07-06 22:06:58 -0600

    note to josh

--- bity-8/src/cartridge.rs
+++ bity-8/src/cartridge.rs
@@ -1,25 +1,6 @@
-// NOTE: This comment block will be deleted soon. It is Alan's rant.
-// there are two ideas for reading the cartridge.
-// One idea is to put it all into a data structure, and that structure can manipulate things.
-// The other idea is to keep it all in memory and just reference the memory when wanting to do
-// something.
-//
-// I think we want the memory thing, because the developer's code should see what the engine sees.
-// Nothing should change in the read only portion except on startup. So the first thing that the
-// cartridge should do is copy the file into the read only section.
-//
-// Let's do that then.
-
-// So, do I still want to store all the offsets in a header data structure?
-// Or do I want to just have functions that return the offsets? In theory, the readonly cartridge
-// section won't ever change, so it would be fine to have a header thing. But, what about when
-// multi-cart functionality is included? Then we would have a problem. Unless we figured out the
-// header information.
-//
-// And if someone were able to change the read only section, then they could change the data in it,
-// which should be fine. We should be assuming that all the information is contained in memory and
-// that the program doesn't store "hidden variables". This is emulating fantasy hardware. So there
-// should just be functions that gives you each thing.
+// NOTE: FOR JOSH
+// This is what I started doing for reading the cartridge. You can change whatever if you wanna
+// make tools or sumthin here. You prob know what you want better than me.
 
 use std::io;
 use std::io::prelude::*;
