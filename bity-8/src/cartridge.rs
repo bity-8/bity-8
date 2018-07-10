f6915cff69405a8ac6a55cbd79fef21d7b423183
joshbackstein <josh.backstein@gmail.com>
2018-07-10 01:53:19 -0600

    Read Lua code from compiled cartridge

--- bity-8/src/cartridge.rs
+++ bity-8/src/cartridge.rs
@@ -10,6 +10,8 @@ use std::str;
 
 use memory as mem;
 
+const HEADER_SIZE: usize = 0x20;
+
 pub struct Version {
     major: u8,
     minor: u8
@@ -37,17 +39,45 @@ pub fn get_code_string() -> String {
     // TODO: make this more professional for the cartridge, and use the offsets specified at the
     // beginning.
 
-    // for now, we will go until a null character.
-    let mut ind = 0;
-    let buffer = mem::get_area(mem::LOC_CART);
-    for i in 0..buffer.len() {
-        if buffer[i] == 0 {
-            ind = i;
-            break;
+    // By default, code goes until beginning of sprite data
+    let mut offset_buffer =
+        mem::get_sub_area(mem::LOC_CART, mem::OFF_SPRITE_DATA);
+    let mut end = offset_buffer[2] as usize;
+    end |= (offset_buffer[1] as usize) << 8;
+    end |= (offset_buffer[0] as usize) << 16;
+
+    // If sprite data doesn't exist, code goes until beginning of tile data
+    if end == 0 {
+        offset_buffer = mem::get_sub_area(mem::LOC_CART, mem::OFF_TILE_DATA);
+        end = offset_buffer[2] as usize;
+        end |= (offset_buffer[1] as usize) << 8;
+        end |= (offset_buffer[0] as usize) << 16;
+    }
+
+    // If tile data doesn't exist, code goes until beginning of audio data
+    if end == 0 {
+        offset_buffer = mem::get_sub_area(mem::LOC_CART, mem::OFF_AUDIO_DATA);
+        end = offset_buffer[2] as usize;
+        end |= (offset_buffer[1] as usize) << 8;
+        end |= (offset_buffer[0] as usize) << 16;
+    }
+
+    // If audio data doesn't exist, code goes until end of cartridge file
+    let cartridge_buffer = mem::get_area(mem::LOC_CART);
+    if end == 0 {
+        // Code is the only thing in the cartridge, so either a null character
+        // or the end of the cartridge will indicate the end of the code section
+        end = cartridge_buffer.len();
+        for i in HEADER_SIZE..end {
+            if cartridge_buffer[i] == 0 {
+                end = i;
+                break;
+            }
         }
     }
 
-    let s = str::from_utf8(&buffer[0..ind]).unwrap();
+    // Ignore header and only read up to end of code section
+    let s = str::from_utf8(&cartridge_buffer[HEADER_SIZE..end]).unwrap();
     let mut s = String::from(s);
     println!("{}", s);
     s
