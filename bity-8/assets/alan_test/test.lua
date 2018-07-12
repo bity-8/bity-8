a61e7e742072edb41afcdac2895ea4a1bc8f5bd2
Alan Morgan <alanxoc3@gmail.com>
2018-07-11 20:52:26 -0600

    trying to clean up sound, but still not that clean.

--- bity-8/assets/alan_test/test.lua
+++ bity-8/assets/alan_test/test.lua
@@ -14,17 +14,17 @@ function _update()
    if btn(7) then play_sfx(0, 4) end
    if btn(6) then inst = math.floor(math.random() * 4) * 16 + 0xA end
 
-   if btn(0) then _poke(0x40035, 0x40) _poke(0x40034, inst)
-   else _poke(0x40034, 0x00) end -- chan 1
+   -- if btn(0) then _poke(0x40035, 0x40) _poke(0x40034, inst)
+   -- else _poke(0x40034, 0x00) end -- chan 1
 
-   if btn(1) then _poke(0x40039, 0x42) _poke(0x40038, inst)
-   else _poke(0x40038, 0x00) end -- chan 2
+   -- if btn(1) then _poke(0x40039, 0x42) _poke(0x40038, inst)
+   -- else _poke(0x40038, 0x00) end -- chan 2
 
-   if btn(2) then _poke(0x4003D, 0x44) _poke(0x4003C, inst)
-   else _poke(0x4003C, 0x00) end -- chan 3
+   -- if btn(2) then _poke(0x4003D, 0x44) _poke(0x4003C, inst)
+   -- else _poke(0x4003C, 0x00) end -- chan 3
 
-   if btn(3) then _poke(0x40041, 0x47) _poke(0x40040, inst)
-   else _poke(0x40040, 0x00) end -- chan 4
+   -- if btn(3) then _poke(0x40041, 0x47) _poke(0x40040, inst)
+   -- else _poke(0x40040, 0x00) end -- chan 4
 
    -- set the screen.
    for i=0, 192-1 do
